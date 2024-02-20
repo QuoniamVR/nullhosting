16e8548d3910deadf54b01db80a803fb04cb71ce
Spencer Anderson <spkoanderson@outlook.com>
2024-02-19 21:15:21 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,1 @@
-<?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-define('DB_SERVER', '127.0.0.1');
-define('DB_USERNAME', 'pbfutbsd');
-define('DB_PASSWORD', 'mHVqJkF8vS5wcE');
-define('DB_NAME', 'pbfutbsd_hosting');
- 
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-?>
+
